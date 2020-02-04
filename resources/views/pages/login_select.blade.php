977b0222923c3693556b7c0550fd976594d6c639
John Derick De Leon <john07@simplexi.com.ph>
2020-02-04 16:07:25 +0800

    Upgrade bootstrap version to 4.4.1 from 4.2.1. Add style on login css to center login selector. Working login selector. Needs to upgrade the buttons

--- resources/views/pages/login_select.blade.php
+++ resources/views/pages/login_select.blade.php
@@ -6,9 +6,36 @@
 @endpush
 
 @section('content')
-    <div id="app">
-        <div class="wrapper">
-             
+    <div id="app" class="h-100">
+        <div class="wrapper center-container">
+            <div class="container text-center absolute-center">
+                <figure class="logo">
+                    <img src="/img/TIP-logo.png">
+                </figure>
+                <div class="row">
+                    <div class="col-sm-4 text-white text-center mt-5">
+                        <div class="card mx-auto" style="width:80%">
+                            <div class="card-body">
+                                <a href="/admin" class="stretched-link">Admin</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 text-white text-center mt-5">
+                        <div class="card mx-auto" style="width:80%;">
+                            <div class="card-body">
+                                <a href="/teacher" class="stretched-link">Teacher</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 text-white text-center mt-5">
+                        <div class="card mx-auto" style="width:80%;">
+                            <div class="card-body">
+                                <a href="/student" class="stretched-link">Student</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
