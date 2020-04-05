186774dd51d7825218a139e92e599559d42851b3
John Derick De Leon <john07@simplexi.com.ph>
2020-04-05 19:21:51 +0800

    Fixed method type to GET

--- routes/web.php
+++ routes/web.php
@@ -122,4 +122,4 @@ Route::post('/assign', 'App\controllerUsers@assignTeacherIntegration');
 Route::get('/assigned/instructors', 'IntegratedCourses\controllerIntegratedCourses@getAssignedInstructors');
 
 Route::post('/compute', 'Computation\controllerComputation@startCompute');
-Route::post('/quizzes/graph/{teacher_id}', 'Quizzes\controllerQuizzesGraph@getQuizzesGraph');
+Route::get('/quizzes/graph/{teacher_id}', 'Quizzes\controllerQuizzesGraph@getQuizzesGraph');
